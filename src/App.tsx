3a60d733b19cf2710e9b432216f57d707ad1e224
Evanilson <evanilsonmanoel85@gmail.com>
2023-10-03 21:49:44 -0300

    modo dark remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Header } from "./components/header"
 import { Main } from "./components/main"
 import { Footer } from "./components/footer"
@@ -6,17 +5,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; 
 
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./themes";
+import { lightTheme } from "./themes";
 
 import { Wrapper } from './styles/globalStyles';
 
 
 function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   return (
     <>
-      <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <ThemeProvider theme={lightTheme}>
         <Wrapper>
           <Header />
           <Main />
